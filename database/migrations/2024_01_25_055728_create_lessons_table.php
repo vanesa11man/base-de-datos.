dc5683717945af2bccbfb3cad02c4f286c02ee70
vanesa11man <vanesamendozayu11@gmail.com>
2024-01-30 15:28:43 -0500

    primer compromiso

---
+++ database/migrations/2024_01_25_055728_create_lessons_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 150);
+            $table->text('description')->max(400);
+            $table->string('image_uri' , 255)->nullable();
+            $table->string('content_uri', 255);
+            $table->string('pdf_uri',255);
+            $table->unsignedBigInteger('level_id');
+            $table->timestamps();
+
+            $table->foreign('level_id')
+            ->references('id')->on('levels')
+            ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
